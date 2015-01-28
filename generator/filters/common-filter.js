07d522b4e07c06ca062796a4825675063d16ca25
syaku <secon@aintop.co.kr>
2015-01-28 12:05:18 +0900

    1차 완료

--- generator/filters/common-filter.js
+++ generator/filters/common-filter.js
@@ -1,7 +1,14 @@
 app.filter('frist_uppercase', function() {
 	return function(inputValue) {
 		if (inputValue === undefined) { inputValue = ''; }
-		var capitalized = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
-		return capitalized;
+
+		if (inputValue.indexOf('_') > -1) {
+			var div = inputValue.split('_');
+			var capitalized = div[0].charAt(0).toUpperCase() + div[0].substring(1) + div[1].charAt(0).toUpperCase() + div[1].substring(1);
+			return capitalized;
+		} else {
+			var capitalized = inputValue.charAt(0).toUpperCase() + inputValue.substring(1);
+			return capitalized;
+		}
 	};
 });
